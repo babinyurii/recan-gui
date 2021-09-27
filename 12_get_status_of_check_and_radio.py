7b5467a3060b7ce8ef89b7fc33f113b5257f7e75
Yuriy Babin <babin.yurii@gmail.com>
2021-09-27 16:02:07 +0300

    Add files via upload

---
+++ 12_get_status_of_check_and_radio.py
@@ -0,0 +1,188 @@
+import PySimpleGUI as sg
+from simgen import Simgen
+
+
+sg.theme('DarkAmber')  
+
+"""
+    Embedding the Matplotlib toolbar into your application
+"""
+# ------------------------------- This is to include a matplotlib figure in a Tkinter canvas
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+
+
+def draw_figure_w_toolbar(canvas, fig, canvas_toolbar):
+    if canvas.children:
+        for child in canvas.winfo_children():
+            child.destroy()
+    if canvas_toolbar.children:
+        for child in canvas_toolbar.winfo_children():
+            child.destroy()
+    figure_canvas_agg = FigureCanvasTkAgg(fig, master=canvas)
+    figure_canvas_agg.draw()
+    toolbar = Toolbar(figure_canvas_agg, canvas_toolbar)
+    toolbar.update()
+    figure_canvas_agg.get_tk_widget().pack(side='right', fill='both', expand=1)
+
+class Toolbar(NavigationToolbar2Tk):
+    def __init__(self, *args, **kwargs):
+        super(Toolbar, self).__init__(*args, **kwargs)
+
+
+# ------------------------------- PySimpleGUI CODE
+
+layout = [
+    [sg.FileBrowse(), sg.Button("get seq names", key="-get_seq_names-")], 
+    
+    [sg.Frame("list of sequences", [ [sg.T()] ], key="-list_of_seq-")],
+    
+    [sg.Text("sliding window size"), sg.Input()],
+    [sg.Text("window shift (nuc)"), sg.Input()],
+    [sg.Text("potential recombinant number"), sg.Input()],
+    [sg.Button('Plot', key="-plot-")],
+    [sg.T('Controls:')],
+    [sg.Canvas(key='controls_cv')],
+    [sg.Column(
+        layout=[
+            [sg.Canvas(key='fig_cv',
+                       # it's important that you set this size
+                       size=(400 * 2, 400)
+                       )]
+        ],
+        background_color='#DAE0E6',
+        pad=(0, 0)
+    )],
+    [sg.MLine(key='-ML-'+sg.WRITE_ONLY_KEY, size=(150, 10))]
+]
+
+
+window = sg.Window('12', layout, resizable=True)
+
+#output to multiline
+print = lambda *args, **kwargs: window['-ML-'+ sg.WRITE_ONLY_KEY].print(*args, **kwargs)
+
+
+seq_names = []
+sequences_added = False # if data are retrieved from file path
+#potential_recombinant = None 
+#sequenced_to_draw = []
+#checkboxes_and_radiobuttons = []
+
+
+
+
+def get_checkboxes_and_radiobuttons(values):
+    for key in values.keys():
+        if type(key) == str:
+            if key.endswith("checkbox") or key.endswith("radio"):
+                checkboxes_and_radiobuttons.append(key) 
+    return checkboxes_and_radiobuttons
+
+
+
+def get_pot_rec_id(checkboxes_and_radiobuttons, values):
+    for seq_id in checkboxes_and_radiobuttons:
+        if seq_id.endswith("radio"):
+            if values[seq_id]:
+                potential_recombinant = seq_id.rsplit("_", maxsplit=1)[0]
+    return potential_recombinant
+
+
+
+def get_seq_ids_to_draw(checkboxes_and_radiobuttons, values, sequences_to_draw):
+    for seq_id in checkboxes_and_radiobuttons:
+        if seq_id.endswith("checkbox"):
+            if values[seq_id]:
+                sequences_to_draw.append(seq_id.rsplit("_", maxsplit=1)[0])
+    return sequences_to_draw
+
+
+while True:
+        
+    try: 
+        event, values = window.read()
+        #print(event, values)
+        if event in (sg.WIN_CLOSED, 'Exit'):  # always,  always give a way out!
+            break
+        
+        elif event == "-get_seq_names-":
+            # create sim obj which inherets from multiple alignment from biopython
+            sim_obj = Simgen(values["Browse"])
+            
+            # #############################
+            # RETRIEVE THE DATA FROM ALIGNMENT
+            # get alignment length
+            counter = 0
+            for record in sim_obj:
+                seq_names.append(record.id)
+                counter += 1
+            print("total number of seq in alignment: ", counter)
+            print("alignment length: ", sim_obj.get_alignment_length())
+            
+            if not sequences_added:
+                for seq_id in seq_names:
+                    window.extend_layout(window['-list_of_seq-'], 
+                                         [[sg.Radio("", 
+                                                    group_id="-potential_recombinant-", 
+                                                    key=seq_id + "_radio"), 
+                                                                                                                                    
+                                           sg.Checkbox("", key=seq_id + "_checkbox"), 
+                                           sg.Text(seq_id)]])
+                sequences_added = True       
+            print(event, values)
+            print("sequences added: ", sequences_added)
+            
+        elif event == '-plot-':
+            #potential_recombinant = None
+            sequences_to_draw = []
+            checkboxes_and_radiobuttons = []
+            
+            checkboxes_and_radiobuttons = get_checkboxes_and_radiobuttons(values)
+            for i in checkboxes_and_radiobuttons:
+                print(i)
+            
+            potential_recombinant = get_pot_rec_id(checkboxes_and_radiobuttons, values)
+            print("potential recombinant: ", potential_recombinant)
+            
+            sequences_to_draw = get_seq_ids_to_draw(checkboxes_and_radiobuttons, values, 
+                                                    sequences_to_draw)
+            print("sequences to draw: ")
+            for i in sequences_to_draw:
+                print(i)
+           
+
+            
+            #########################################################
+            # ------------------------------- PASTE YOUR MATPLOTLIB CODE HERE
+            plt.close()
+            plt.figure(1)
+            fig = plt.gcf()
+            DPI = fig.get_dpi()
+            # ------------------------------- you have to play with this size to reduce the movement error when the mouse hovers over the figure, it's close to canvas size
+            fig.set_size_inches(404 * 2 / float(DPI), 404 / float(DPI))
+            # -------------------------------
+            
+            #sim_obj = Simgen(values["Browse"])
+            sliding_window_size= int(values[0])
+            window_shift = int(values[1])
+            pot_rec_index = int(values[2])
+            
+            sim_obj.simgen(pot_rec=pot_rec_index, window=sliding_window_size, shift=window_shift)
+        
+            plt.title("")
+            plt.xlabel('alignment position')
+            plt.ylabel('distance')
+            plt.grid()
+    
+            # ------------------------------- Instead of plt.show()
+            draw_figure_w_toolbar(window['fig_cv'].TKCanvas, fig, window['controls_cv'].TKCanvas)
+    
+    except Exception as ex:
+            print("exception : ", ex, "\n", "-" * 50)
+
+
+window.close()
+
+
+
